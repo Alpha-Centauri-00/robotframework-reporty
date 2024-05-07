497e96d78cca2bb63967fa530abb962590aaaaca
alpha <alpha.com>
2024-05-07 10:05:40 +0200

    edit wrong path xml

--- src/pages/page1.py
+++ src/pages/page1.py
@@ -2,6 +2,7 @@ from base_app import baseApp
 from navigation import Navi
 import data_base as data_base
 import xml_parser as xml_parser
+from robot.errors import DataError
 import os
 
 class page1(baseApp):
@@ -19,59 +20,63 @@ class page1(baseApp):
         self.create_write("# test results! Reporty")
         
         xml = self.create_text_input("Load xml-Results",".xml file path e.g. ../full-path/results/output.xml")
-        if xml:
-            test_result = xml_parser.TestResult(xml)
-            ## Metric
-            col1, col2, col3, col4 = self.create_columns(4)
-            with col1:
-                self.create_metric("Test results","Pass",f"{ test_result.passed }")
-            with col2:
-                self.create_metric("Test results","Failed",f"{ -test_result.failed }")
-            with col3:
-                self.create_metric("Test results","Skipped",f"{ test_result.skipped }",delta_color="off")  
-            with col4:
-                self.create_metric("Test results","Total",f"{ test_result.total }",delta_color="off")
+        try:
+            if xml:
+                test_result = xml_parser.TestResult(xml)
+                ## Metric
+                col1, col2, col3, col4 = self.create_columns(4)
+                with col1:
+                    self.create_metric("Test results","Pass",f"{ test_result.passed }")
+                with col2:
+                    self.create_metric("Test results","Failed",f"{ -test_result.failed }")
+                with col3:
+                    self.create_metric("Test results","Skipped",f"{ test_result.skipped }",delta_color="off")  
+                with col4:
+                    self.create_metric("Test results","Total",f"{ test_result.total }",delta_color="off")
 
-            self.create_dataframe(
-                {
-                "Test Name": test_result.testsuite_names,
-                "Test Case Name": test_result.testcase_names,
-                "Status": test_result.testcases_status,
-                "Elapsed": test_result.testcases_elapsed
-                },
-                width=400,
-                height=400,
-                container_width=True
-            )
-            if test_result and self.create_button("Save To DB"):
+                self.create_dataframe(
+                    {
+                    "Test Name": test_result.testsuite_names,
+                    "Test Case Name": test_result.testcase_names,
+                    "Status": test_result.testcases_status,
+                    "Elapsed": test_result.testcases_elapsed
+                    },
+                    width=400,
+                    height=400,
+                    container_width=True
+                )
+                if test_result and self.create_button("Save To DB"):
 
 
-                self.on_save_to_db_clicked("test.db", test_result)
-                self.create_toast("Test results saved to database successfully.")
-                               
+                    self.on_save_to_db_clicked("test.db", test_result)
+                    self.create_toast("Test results saved to database successfully.")
 
                 
-        else:
-            current_dir = os.getcwd()
-            db_files = [file for file in os.listdir(current_dir) if file.endswith(".db")]
-            with self.create_spinner("Wait for it..."):
-                #time.sleep(1)
-                if db_files:
-            
-                    self.create_dataframe(
-                        {
-                            "Test Name": data_base.load_db_file("test.db").execute("SELECT test_suite_name FROM test_results").fetchall(),
-                            "Test Case Name": data_base.load_db_file("test.db").execute("SELECT test_case_name FROM test_results").fetchall(),
-                            "Status": data_base.load_db_file("test.db").execute("SELECT test_case_status FROM test_results").fetchall(),
-                            "Elapsed": data_base.load_db_file("test.db").execute("SELECT test_case_elapsed_time FROM test_results").fetchall()
-                        },
-                        width=400,
-                        height=400,
-                        container_width=True
-                    )
-                else:
-                    pass
+            else:
+                current_dir = os.getcwd()
+                db_files = [file for file in os.listdir(current_dir) if file.endswith(".db")]
+                with self.create_spinner("Wait for it..."):
+                    #time.sleep(1)
+                    if db_files:
+                
+                        self.create_dataframe(
+                            {
+                                "Test Name": data_base.load_db_file("test.db").execute("SELECT test_suite_name FROM test_results").fetchall(),
+                                "Test Case Name": data_base.load_db_file("test.db").execute("SELECT test_case_name FROM test_results").fetchall(),
+                                "Status": data_base.load_db_file("test.db").execute("SELECT test_case_status FROM test_results").fetchall(),
+                                "Elapsed": data_base.load_db_file("test.db").execute("SELECT test_case_elapsed_time FROM test_results").fetchall()
+                            },
+                            width=400,
+                            height=400,
+                            container_width=True
+                        )
+                    else:
+                        pass
 
+        except DataError as e:
+
+            self.create_warning(f"An error occurred: {e} ⚠️")
+            print(e)
         
 
 
